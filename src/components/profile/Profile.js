adb284158e95e2fdb0034c6a9bdcc07e30d48399
Quanmuito <49555971+Quanmuito@users.noreply.github.com>
2020-11-21 01:49:02 +0200

    test deploy

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -8,6 +8,7 @@ export default function Profile() {
     const { id } = useParams();
     console.log(id);
     const [user, setUser] = useState([]);
+    console.log(user);
 
     useEffect(() => {
         fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
@@ -15,50 +16,51 @@ export default function Profile() {
         .then(json => setUser(json))
     }, [id])
 
-    if (user.length !== 0) {
-        return (
-            <div>
-                <Picture name={ user.name } />
+    // if (user.length !== 0) {
+    //     return (
+    //         <div>
+    //             <Picture name={ user.name } />
 
-                <Detail
-                    isOpen={true}
-                    title='Personal information'
-                    info={{
-                        Name: user.name,
-                        Username: user.username,
-                        Email: user.email,
-                        Phone: user.phone,
-                        Website: user.website
-                    }}
-                />
+    //             <Detail
+    //                 isOpen={true}
+    //                 title='Personal information'
+    //                 info={{
+    //                     Name: user.name,
+    //                     Username: user.username,
+    //                     Email: user.email,
+    //                     Phone: user.phone,
+    //                     Website: user.website
+    //                 }}
+    //             />
                 
-                <br></br>
+    //             <br></br>
 
-                <Detail
-                    isOpen={false}
-                    title='Address'
-                    info={{
-                        Street: user.address.street,
-                        Suite: user.address.suite,
-                        City: user.address.city,
-                        Zipcode: user.address.zipcode
-                    }}
-                />
+    //             <Detail
+    //                 isOpen={false}
+    //                 title='Address'
+    //                 info={{
+    //                     Street: user.address.street,
+    //                     Suite: user.address.suite,
+    //                     City: user.address.city,
+    //                     Zipcode: user.address.zipcode
+    //                 }}
+    //             />
 
-                <br></br>
+    //             <br></br>
 
-                <Detail
-                    isOpen={false}
-                    title='Company'
-                    info={{
-                        Title: user.company.name,
-                        Description: user.company.catchPhrase + user.company.bs,
-                    }}
-                />
-            </div>
-        )
-    }
-    else {
-        return (<></>)
-    }
+    //             <Detail
+    //                 isOpen={false}
+    //                 title='Company'
+    //                 info={{
+    //                     Title: user.company.name,
+    //                     Description: user.company.catchPhrase + user.company.bs,
+    //                 }}
+    //             />
+    //         </div>
+    //     )
+    // }
+    // else {
+    //     return (<div></div>)
+    // }
+    return (<div></div>) 
 }
